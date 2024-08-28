28d31e1827030c5aeaa972e9b1c857ad84d273ba
Anom <anom9152@gmail.com>
2024-08-28 18:34:08 +0500

    fere

---
+++ src/Layout.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route} from 'react-router-dom';
+import { Navigation } from './components/Navigation/Navigation.jsx';
+import { QrCodeGenerator } from './components/Generate/QrCodeGenerator.jsx';
+import { QrCodeScanner } from './components/Scan/QrCodeScanner.jsx';
+import { GenerateHistory } from './components/GenerateHistory.jsx';
+import { ScanHistory } from './components/ScanHistory.jsx';
+
+const Layout = () => {
+    return (
+        <div>
+            <Navigation />
+
+            
+
+            {/* <QrCodeGenerator /> */}
+
+            {/* <QrCodeScanner /> */}
+
+      <Routes>
+          <Route path="/generate" element={<QrCodeGenerator />} />
+          <Route path="/scan" element={<QrCodeScanner />} />
+          <Route path="/generateHistory" element={< GenerateHistory/>} />
+          <Route path="/scanHistory" element={<ScanHistory />} />
+      </Routes>
+        </div>
+    );
+};
+
+export { Layout };
