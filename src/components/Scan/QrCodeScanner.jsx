d3a0457564ba1f5d48aefcd6651a61241924e6d8
Anomyss868 <anom9152@gmail.com>
2024-08-29 16:15:33 +0500

    Update QrCodeScanner.jsx

--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -13,11 +13,10 @@ export const QrCodeScanner = () => {
     const [scanned, setScanned] = useState(null);
 
     const scanHandler = (result) => {
-        if (!result) return;
+        
 
         const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
 
-        if (prevData.includes(result.text)) return;
 
         setScanned(result.text);
 
@@ -30,9 +29,8 @@ export const QrCodeScanner = () => {
     return (
         <div className={s.container}>
             <QrReader
-                scanDelay={1000}
                 onResult={scanHandler}
-                containerStyle={{ width: '500px' }}
+                containerStyle={{ width: '500px', height: '300px' }}
             />
         
             <p className={s.result}>{scanned}</p>
