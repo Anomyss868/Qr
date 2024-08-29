2ff4de9b6ed448a7de8cdcfe4bd64b98f9888d89
Anomyss868 <anom9152@gmail.com>
2024-08-29 15:59:13 +0500

    Update QrCodeScanner.jsxr

--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -6,35 +6,39 @@ import { SCAN_DATA } from '../../contacts';
 import { json } from 'react-router-dom';
 
 
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+import s from './qrCodeScanner.module.css';
+
+
+
 export const QrCodeScanner = () => {
-     const [scanned, setScanned] = useState(null);
+    const [scanned, setScanned] = useState(null);
 
     const scanHandler = (result) => {
-        setScanned(result[0].rawValue)
+        if (!result) return;
 
         const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
 
-       
+        if (prevData.includes(result.text)) return;
+
+        setScanned(result.text);
+
         localStorage.setItem(
-          SCAN_DATA, 
-          JSON.stringify([...prevData, result[0].rawValue]))
+            SCAN_DATA,
+            JSON.stringify([...prevData, result.text])
+        );
     };
-
-
-
 
     return (
         <div className={s.container}>
-
-<QrReader
-        onResult={(result, error) => {
-          console.log(result)
-        }}
-       containerStyle={{ width: '500px', height: '300px' }}
-      />
-            
-            
-            <p className={s.result}></p>   
+            <QrReader
+                scanDelay={1000}
+                onResult={scanHandler}
+                containerStyle={{ width: '500px' }}
+            />
+        
+            <p className={s.result}>{scanned}</p>
         </div>
-    )
-}
+    );
+};
